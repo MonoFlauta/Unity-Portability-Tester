4e6de8862d798efef538653d00c2b85b12484afc
Facundo Balboa <monoflauta@gmail.com>
2021-07-06 21:31:28 -0300

    Created Portability Checker

---
+++ Assets/UnityPortabilityTester/Editor/PortabilityChecker.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Linq;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace UnityPortabilityTester.Editor
+{
+    public class PortabilityChecker
+    {
+        private readonly UnityPortabilityTesterSettings _settings;
+        private readonly Action<PrefabToTest, GameObject, string> _onPrefabFail;
+        private readonly Action<Object, PrefabToTest, Type> _onComponentFail;
+
+        public PortabilityChecker(
+            UnityPortabilityTesterSettings settings, 
+            Action<PrefabToTest, GameObject, string> onPrefabFail, 
+            Action<Object, PrefabToTest, Type> onComponentFail
+            )
+        {
+            _settings = settings;
+            _onPrefabFail = onPrefabFail;
+            _onComponentFail = onComponentFail;
+        }
+
+        public void CheckPortabilityFor(PrefabToTest prefabToTest)
+        {
+            if (_settings.checkText) CheckTextComponentsFor(prefabToTest);
+            if (_settings.checkTextMeshProUI) CheckTextMeshProUIComponentsFor(prefabToTest);
+            if (_settings.checkImages) CheckImageComponentsFor(prefabToTest);
+            if (_settings.checkRawImages) CheckRawImageComponentsFor(prefabToTest);
+            if (_settings.checkButtons) CheckButtonComponentsFor(prefabToTest);
+            if (_settings.checkToggles) CheckToggleComponentsFor(prefabToTest);
+            if (_settings.checkSpriteSwap) CheckScrollBarComponentsFor(prefabToTest);
+            if (_settings.checkDropdowns) CheckDropdownComponentsFor(prefabToTest);
+            if (_settings.checkInputFields) CheckInputFieldComponentsFor(prefabToTest);
+
+            if (_settings.checkPrefabs) CheckPrefabsFor(prefabToTest);
+
+            if (_settings.checkAnimators) CheckAnimatorComponentsFor(prefabToTest);
+
+            if (_settings.checkSpriteRenderers) CheckSpriteRendererComponentsFor(prefabToTest);
+            if (_settings.checkSpriteMasks) CheckSpriteMaskComponentsFor(prefabToTest);
+            if (_settings.checkCollider2D) CheckColliders2DComponentsFor(prefabToTest);
+
+            if (_settings.checkMeshFilter) CheckMeshFilterComponentsFor(prefabToTest);
+            if (_settings.checkMeshRenderer) CheckMeshRendererComponentsFor(prefabToTest);
+            if (_settings.checkColliders) CheckCollidersComponentsFor(prefabToTest);
+            if (_settings.checkTextMeshPro) CheckTextMeshProComponentsFor(prefabToTest);
+        }
+
+        private void CheckTextMeshProComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var textMesh in prefabToTest.prefab.GetComponentsInChildren<TextMeshPro>())
+            {
+                CheckPathFor(textMesh.mesh, prefabToTest, typeof(TextMeshPro));
+                CheckPathFor(textMesh.font, prefabToTest, typeof(TextMeshPro));
+            }
+        }
+
+        private void CheckColliders2DComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var collider in prefabToTest.prefab.GetComponentsInChildren<Collider2D>())
+            {
+                CheckPathFor(collider.sharedMaterial, prefabToTest, typeof(Collider2D));
+            }
+        }
+
+        private void CheckCollidersComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var collider in prefabToTest.prefab.GetComponentsInChildren<Collider>())
+            {
+                CheckPathFor(collider.material, prefabToTest, typeof(Collider));
+                CheckPathFor(collider.sharedMaterial, prefabToTest, typeof(Collider));
+            }
+        }
+
+        private void CheckMeshRendererComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var meshRenderer in prefabToTest.prefab.GetComponentsInChildren<MeshRenderer>())
+            {
+                CheckPathFor(meshRenderer.material, prefabToTest, typeof(MeshRenderer));
+                CheckPathFor(meshRenderer.sharedMaterial, prefabToTest, typeof(MeshRenderer));
+                foreach (var material in meshRenderer.materials)
+                    CheckPathFor(material, prefabToTest, typeof(MeshRenderer));
+
+                foreach (var sharedMaterial in meshRenderer.sharedMaterials)
+                    CheckPathFor(sharedMaterial, prefabToTest, typeof(MeshRenderer));
+            }
+        }
+
+        private void CheckMeshFilterComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var meshFilter in prefabToTest.prefab.GetComponentsInChildren<MeshFilter>())
+            {
+                CheckPathFor(meshFilter.mesh, prefabToTest, typeof(MeshFilter));
+                CheckPathFor(meshFilter.sharedMesh, prefabToTest, typeof(MeshFilter));
+            }
+        }
+
+        private void CheckSpriteMaskComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var spriteMask in prefabToTest.prefab.GetComponentsInChildren<SpriteMask>())
+                CheckPathFor(spriteMask.sprite, prefabToTest, typeof(SpriteMask));
+        }
+
+        private void CheckSpriteRendererComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var spriteRenderer in prefabToTest.prefab.GetComponentsInChildren<SpriteRenderer>())
+            {
+                CheckPathFor(spriteRenderer.sprite, prefabToTest, typeof(SpriteRenderer));
+                CheckPathFor(spriteRenderer.sharedMaterial, prefabToTest, typeof(SpriteRenderer));
+                foreach (var sharedMaterial in spriteRenderer.sharedMaterials)
+                    CheckPathFor(sharedMaterial, prefabToTest, typeof(SpriteRenderer));
+            }
+        }
+
+        private void CheckAnimatorComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var animator in prefabToTest.prefab.GetComponentsInChildren<Animator>())
+            {
+                CheckPathFor(animator.runtimeAnimatorController, prefabToTest, typeof(Animator));
+                CheckPathFor(animator.avatar, prefabToTest, typeof(Animator));
+                CheckAnimatorController(animator.runtimeAnimatorController, prefabToTest);
+            }
+        }
+
+        private void CheckAnimatorController(RuntimeAnimatorController controller, PrefabToTest prefabToTest)
+        {
+            foreach (var clip in controller.animationClips)
+                CheckPathFor(clip, prefabToTest, typeof(AnimationClip));
+        }
+
+        private void CheckInputFieldComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var inputField in prefabToTest.prefab.GetComponentsInChildren<InputField>())
+            {
+                if (inputField.transition != Selectable.Transition.SpriteSwap) continue;
+                CheckPathFor(inputField.spriteState.highlightedSprite, prefabToTest, typeof(InputField));
+                CheckPathFor(inputField.spriteState.pressedSprite, prefabToTest, typeof(InputField));
+                CheckPathFor(inputField.spriteState.selectedSprite, prefabToTest, typeof(InputField));
+                CheckPathFor(inputField.spriteState.disabledSprite, prefabToTest, typeof(InputField));
+            }
+        }
+
+        private void CheckDropdownComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var dropdown in prefabToTest.prefab.GetComponentsInChildren<Dropdown>())
+            {
+                if (dropdown.transition != Selectable.Transition.SpriteSwap) continue;
+                CheckPathFor(dropdown.spriteState.highlightedSprite, prefabToTest, typeof(Dropdown));
+                CheckPathFor(dropdown.spriteState.pressedSprite, prefabToTest, typeof(Dropdown));
+                CheckPathFor(dropdown.spriteState.selectedSprite, prefabToTest, typeof(Dropdown));
+                CheckPathFor(dropdown.spriteState.disabledSprite, prefabToTest, typeof(Dropdown));
+            }
+        }
+
+        private void CheckScrollBarComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var scrollbar in prefabToTest.prefab.GetComponentsInChildren<Scrollbar>())
+            {
+                if (scrollbar.transition != Selectable.Transition.SpriteSwap) continue;
+                CheckPathFor(scrollbar.spriteState.highlightedSprite, prefabToTest, typeof(Scrollbar));
+                CheckPathFor(scrollbar.spriteState.pressedSprite, prefabToTest, typeof(Scrollbar));
+                CheckPathFor(scrollbar.spriteState.selectedSprite, prefabToTest, typeof(Scrollbar));
+                CheckPathFor(scrollbar.spriteState.disabledSprite, prefabToTest, typeof(Scrollbar));
+            }
+        }
+
+        private void CheckToggleComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var toggle in prefabToTest.prefab.GetComponentsInChildren<Toggle>())
+            {
+                if (toggle.transition != Selectable.Transition.SpriteSwap) continue;
+                CheckPathFor(toggle.spriteState.highlightedSprite, prefabToTest, typeof(Toggle));
+                CheckPathFor(toggle.spriteState.pressedSprite, prefabToTest, typeof(Toggle));
+                CheckPathFor(toggle.spriteState.selectedSprite, prefabToTest, typeof(Toggle));
+                CheckPathFor(toggle.spriteState.disabledSprite, prefabToTest, typeof(Toggle));
+            }
+        }
+
+        private void CheckButtonComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var button in prefabToTest.prefab.GetComponentsInChildren<Button>())
+            {
+                if (button.transition != Selectable.Transition.SpriteSwap) continue;
+                CheckPathFor(button.spriteState.highlightedSprite, prefabToTest, typeof(Button));
+                CheckPathFor(button.spriteState.pressedSprite, prefabToTest, typeof(Button));
+                CheckPathFor(button.spriteState.selectedSprite, prefabToTest, typeof(Button));
+                CheckPathFor(button.spriteState.disabledSprite, prefabToTest, typeof(Button));
+            }
+        }
+
+        private void CheckRawImageComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var image in prefabToTest.prefab.GetComponentsInChildren<RawImage>())
+                CheckPathFor(image.texture, prefabToTest, typeof(RawImage));
+        }
+
+        private void CheckImageComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var image in prefabToTest.prefab.GetComponentsInChildren<Image>())
+                CheckPathFor(image.sprite, prefabToTest, typeof(Image));
+        }
+
+        private void CheckTextMeshProUIComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var text in prefabToTest.prefab.GetComponentsInChildren<TextMeshProUGUI>())
+                CheckPathFor(text.font, prefabToTest, typeof(TextMeshProUGUI));
+        }
+
+        private void CheckTextComponentsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var text in prefabToTest.prefab.GetComponentsInChildren<Text>())
+                CheckPathFor(text.font, prefabToTest, typeof(Text));
+        }
+
+        private void CheckPrefabsFor(PrefabToTest prefabToTest)
+        {
+            foreach (var gameObject in prefabToTest.prefab.GetComponentsInChildren<Transform>().Select(x => x.gameObject))
+            {
+                if (!PrefabUtility.IsAnyPrefabInstanceRoot(gameObject)) continue;
+                var path = AssetDatabase.GetAssetPath(gameObject);
+                if (!string.IsNullOrEmpty(path) && !path.Contains(prefabToTest.constraintPath) && !_settings.externalResourcesPaths.Any(x => path.Contains(x))) 
+                    _onPrefabFail(prefabToTest, gameObject, path);
+            }
+        }
+
+        private void CheckPathFor(Object asset, PrefabToTest prefabToTest, Type type)
+        {
+            if(asset != null 
+               && (string.IsNullOrEmpty(AssetDatabase.GetAssetPath(asset).Trim()) || !IsPathValid(AssetDatabase.GetAssetPath(asset), prefabToTest.constraintPath)))
+                _onComponentFail(asset, prefabToTest, type);
+        }
+        
+        private bool IsPathValid(string path, string constraintPath) 
+            => path.Contains(constraintPath) || _settings.externalResourcesPaths.Any(path.Contains);
+    }
+}
--- Assets/UnityPortabilityTester/Editor/UnityPortabilityTest.cs
+++ Assets/UnityPortabilityTester/Editor/UnityPortabilityTest.cs
@@ -17,6 +17,7 @@ namespace UnityPortabilityTester.Editor
         private const string SettingsFileName = "Settings.asset";
 
         private UnityPortabilityTesterSettings _settings;
+        private PortabilityChecker _portabilityChecker;
 
         [SetUp]
         public void SetUp()
@@ -24,6 +25,8 @@ namespace UnityPortabilityTester.Editor
             _settings = AssetDatabase.LoadAssetAtPath<UnityPortabilityTesterSettings>(SettingsFilePath);
             if (_settings == null)
                 _settings = CreateScriptableObjectOf<UnityPortabilityTesterSettings>(SettingsFilePath);
+
+            _portabilityChecker = new PortabilityChecker(_settings, AssertPrefabFailFor, AssertComponentFailFor);
         }
 
         [Test]
@@ -31,7 +34,7 @@ namespace UnityPortabilityTester.Editor
         {
             foreach (var prefabsToTest in FindAssetsByType<PrefabsToTestPortabilityData>())
                 for (var i = prefabsToTest.prefabToTests.Length - 1; i >= 0; i--)
-                    CheckPortabilityFor(prefabsToTest.prefabToTests[i]);
+                    _portabilityChecker.CheckPortabilityFor(prefabsToTest.prefabToTests[i]);
         }
 
         [Test]
@@ -46,7 +49,7 @@ namespace UnityPortabilityTester.Editor
                     .Select(t => new PrefabToTest((GameObject)t.Item1, t.Item2)).ToList();
 
                  foreach (var prefabToTest in prefabToTests) 
-                     CheckPortabilityFor(prefabToTest);
+                     _portabilityChecker.CheckPortabilityFor(prefabToTest);
             }
         }
 
@@ -60,221 +63,15 @@ namespace UnityPortabilityTester.Editor
             return scriptableObject;
         }
 
-        private void CheckPortabilityFor(PrefabToTest prefabToTest)
-        {
-            if (_settings.checkText) CheckTextComponentsFor(prefabToTest);
-            if (_settings.checkTextMeshProUI) CheckTextMeshProUIComponentsFor(prefabToTest);
-            if (_settings.checkImages) CheckImageComponentsFor(prefabToTest);
-            if (_settings.checkRawImages) CheckRawImageComponentsFor(prefabToTest);
-            if (_settings.checkButtons) CheckButtonComponentsFor(prefabToTest);
-            if (_settings.checkToggles) CheckToggleComponentsFor(prefabToTest);
-            if (_settings.checkSpriteSwap) CheckScrollBarComponentsFor(prefabToTest);
-            if (_settings.checkDropdowns) CheckDropdownComponentsFor(prefabToTest);
-            if (_settings.checkInputFields) CheckInputFieldComponentsFor(prefabToTest);
+        private static void AssertPrefabFailFor(PrefabToTest prefabToTest, GameObject gameObject, string path) =>
+            Assert.Fail("The prefab " + prefabToTest.prefab.name + " at " + AssetDatabase.GetAssetPath(prefabToTest.prefab) +
+                        " has a prefab outside the location." +
+                        Environment.NewLine + "The GameObject " + gameObject.name + " is located at " + path);
 
-            if (_settings.checkPrefabs) CheckPrefabsFor(prefabToTest);
-
-            if (_settings.checkAnimators) CheckAnimatorComponentsFor(prefabToTest);
-
-            if (_settings.checkSpriteRenderers) CheckSpriteRendererComponentsFor(prefabToTest);
-            if (_settings.checkSpriteMasks) CheckSpriteMaskComponentsFor(prefabToTest);
-            if (_settings.checkCollider2D) CheckColliders2DComponentsFor(prefabToTest);
-
-            if (_settings.checkMeshFilter) CheckMeshFilterComponentsFor(prefabToTest);
-            if (_settings.checkMeshRenderer) CheckMeshRendererComponentsFor(prefabToTest);
-            if (_settings.checkColliders) CheckCollidersComponentsFor(prefabToTest);
-            if (_settings.checkTextMeshPro) CheckTextMeshProComponentsFor(prefabToTest);
-        }
-
-        private void CheckTextMeshProComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var textMesh in prefabToTest.prefab.GetComponentsInChildren<TextMeshPro>())
-            {
-                CheckPathFor<TextMeshPro>(textMesh.mesh, prefabToTest);
-                CheckPathFor<TextMeshPro>(textMesh.font, prefabToTest);
-            }
-        }
-
-        private void CheckColliders2DComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var collider in prefabToTest.prefab.GetComponentsInChildren<Collider2D>())
-            {
-                CheckPathFor<Collider2D>(collider.sharedMaterial, prefabToTest);
-            }
-        }
-
-        private void CheckCollidersComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var collider in prefabToTest.prefab.GetComponentsInChildren<Collider>())
-            {
-                CheckPathFor<Collider>(collider.material, prefabToTest);
-                CheckPathFor<Collider>(collider.sharedMaterial, prefabToTest);
-            }
-        }
-
-        private void CheckMeshRendererComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var meshRenderer in prefabToTest.prefab.GetComponentsInChildren<MeshRenderer>())
-            {
-                CheckPathFor<MeshRenderer>(meshRenderer.material, prefabToTest);
-                CheckPathFor<MeshRenderer>(meshRenderer.sharedMaterial, prefabToTest);
-                foreach (var material in meshRenderer.materials)
-                    CheckPathFor<MeshRenderer>(material, prefabToTest);
-
-                foreach (var sharedMaterial in meshRenderer.sharedMaterials)
-                    CheckPathFor<MeshRenderer>(sharedMaterial, prefabToTest);
-            }
-        }
-
-        private void CheckMeshFilterComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var meshFilter in prefabToTest.prefab.GetComponentsInChildren<MeshFilter>())
-            {
-                CheckPathFor<MeshFilter>(meshFilter.mesh, prefabToTest);
-                CheckPathFor<MeshFilter>(meshFilter.sharedMesh, prefabToTest);
-            }
-        }
-
-        private void CheckSpriteMaskComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var spriteMask in prefabToTest.prefab.GetComponentsInChildren<SpriteMask>())
-                CheckPathFor<SpriteMask>(spriteMask.sprite, prefabToTest);
-        }
-
-        private void CheckSpriteRendererComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var spriteRenderer in prefabToTest.prefab.GetComponentsInChildren<SpriteRenderer>())
-            {
-                CheckPathFor<SpriteRenderer>(spriteRenderer.sprite, prefabToTest);
-                CheckPathFor<SpriteRenderer>(spriteRenderer.sharedMaterial, prefabToTest);
-                foreach (var sharedMaterial in spriteRenderer.sharedMaterials)
-                    CheckPathFor<SpriteRenderer>(sharedMaterial, prefabToTest);
-            }
-        }
-
-        private void CheckAnimatorComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var animator in prefabToTest.prefab.GetComponentsInChildren<Animator>())
-            {
-                CheckPathFor<Animator>(animator.runtimeAnimatorController, prefabToTest);
-                CheckPathFor<Animator>(animator.avatar, prefabToTest);
-                CheckAnimatorController(animator.runtimeAnimatorController, prefabToTest);
-            }
-        }
-
-        private void CheckAnimatorController(RuntimeAnimatorController controller, PrefabToTest prefabToTest)
-        {
-            foreach (var clip in controller.animationClips)
-                CheckPathFor<AnimationClip>(clip, prefabToTest);
-        }
-
-        private void CheckPrefabsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var gameObject in prefabToTest.prefab.GetComponentsInChildren<Transform>().Select(x => x.gameObject))
-            {
-                if (!PrefabUtility.IsAnyPrefabInstanceRoot(gameObject)) continue;
-                var path = AssetDatabase.GetAssetPath(gameObject);
-                if (!string.IsNullOrEmpty(path) && (!path.Contains(prefabToTest.constraintPath) && !_settings.externalResourcesPaths.Any(x => path.Contains(x))))
-                    Assert.Fail("The prefab "+prefabToTest.prefab.name+" at "+AssetDatabase.GetAssetPath(prefabToTest.prefab)+" has a prefab outside the location."+
-                                Environment.NewLine+"The GameObject "+gameObject.name+" is located at "+path);
-            }
-        }
-
-        private void CheckInputFieldComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var inputField in prefabToTest.prefab.GetComponentsInChildren<InputField>())
-            {
-                if (inputField.transition != Selectable.Transition.SpriteSwap) continue;
-                CheckPathFor<InputField>(inputField.spriteState.highlightedSprite, prefabToTest);
-                CheckPathFor<InputField>(inputField.spriteState.pressedSprite, prefabToTest);
-                CheckPathFor<InputField>(inputField.spriteState.selectedSprite, prefabToTest);
-                CheckPathFor<InputField>(inputField.spriteState.disabledSprite, prefabToTest);
-            }
-        }
-
-        private void CheckDropdownComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var dropdown in prefabToTest.prefab.GetComponentsInChildren<Dropdown>())
-            {
-                if (dropdown.transition != Selectable.Transition.SpriteSwap) continue;
-                CheckPathFor<Dropdown>(dropdown.spriteState.highlightedSprite, prefabToTest);
-                CheckPathFor<Dropdown>(dropdown.spriteState.pressedSprite, prefabToTest);
-                CheckPathFor<Dropdown>(dropdown.spriteState.selectedSprite, prefabToTest);
-                CheckPathFor<Dropdown>(dropdown.spriteState.disabledSprite, prefabToTest);
-            }
-        }
-
-        private void CheckScrollBarComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var scrollbar in prefabToTest.prefab.GetComponentsInChildren<Scrollbar>())
-            {
-                if (scrollbar.transition != Selectable.Transition.SpriteSwap) continue;
-                CheckPathFor<Scrollbar>(scrollbar.spriteState.highlightedSprite, prefabToTest);
-                CheckPathFor<Scrollbar>(scrollbar.spriteState.pressedSprite, prefabToTest);
-                CheckPathFor<Scrollbar>(scrollbar.spriteState.selectedSprite, prefabToTest);
-                CheckPathFor<Scrollbar>(scrollbar.spriteState.disabledSprite, prefabToTest);
-            }
-        }
-
-        private void CheckToggleComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var toggle in prefabToTest.prefab.GetComponentsInChildren<Toggle>())
-            {
-                if (toggle.transition != Selectable.Transition.SpriteSwap) continue;
-                CheckPathFor<Toggle>(toggle.spriteState.highlightedSprite, prefabToTest);
-                CheckPathFor<Toggle>(toggle.spriteState.pressedSprite, prefabToTest);
-                CheckPathFor<Toggle>(toggle.spriteState.selectedSprite, prefabToTest);
-                CheckPathFor<Toggle>(toggle.spriteState.disabledSprite, prefabToTest);
-            }
-        }
-
-        private void CheckButtonComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var button in prefabToTest.prefab.GetComponentsInChildren<Button>())
-            {
-                if (button.transition != Selectable.Transition.SpriteSwap) continue;
-                CheckPathFor<Button>(button.spriteState.highlightedSprite, prefabToTest);
-                CheckPathFor<Button>(button.spriteState.pressedSprite, prefabToTest);
-                CheckPathFor<Button>(button.spriteState.selectedSprite, prefabToTest);
-                CheckPathFor<Button>(button.spriteState.disabledSprite, prefabToTest);
-            }
-        }
-
-        private void CheckRawImageComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var image in prefabToTest.prefab.GetComponentsInChildren<RawImage>())
-                CheckPathFor<RawImage>(image.texture, prefabToTest);
-        }
-
-        private void CheckImageComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var image in prefabToTest.prefab.GetComponentsInChildren<Image>())
-                CheckPathFor<Image>(image.sprite, prefabToTest);
-        }
-
-        private void CheckTextMeshProUIComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var text in prefabToTest.prefab.GetComponentsInChildren<TextMeshProUGUI>())
-                CheckPathFor<TextMeshProUGUI>(text.font, prefabToTest);
-        }
-
-        private void CheckTextComponentsFor(PrefabToTest prefabToTest)
-        {
-            foreach (var text in prefabToTest.prefab.GetComponentsInChildren<Text>())
-                CheckPathFor<Text>(text.font, prefabToTest);
-        }
-
-        private void CheckPathFor<T>(Object asset, PrefabToTest prefabToTest)
-        {
-            if(asset != null 
-               && (string.IsNullOrEmpty(AssetDatabase.GetAssetPath(asset).Trim()) || !IsPathValid(AssetDatabase.GetAssetPath(asset), prefabToTest.constraintPath)))
-                Assert.Fail("The component "+typeof(T)+" inside prefab "+prefabToTest.prefab.name+" that is located at "+AssetDatabase.GetAssetPath(prefabToTest.prefab)+" has an external dependency." +
-                            Environment.NewLine+"The dependency is "+asset.name+" and it should be located inside "+prefabToTest.constraintPath+" or at a external resource path."+
-                            Environment.NewLine+"The dependency is located at "+AssetDatabase.GetAssetPath(asset)+" for now");
-        }
-
-        private bool IsPathValid(string path, string constraintPath) 
-            => path.Contains(constraintPath) || _settings.externalResourcesPaths.Any(path.Contains);
+        private static void AssertComponentFailFor(Object asset, PrefabToTest prefabToTest, Type type) =>
+            Assert.Fail("The component "+type+" inside prefab "+prefabToTest.prefab.name+" that is located at "+AssetDatabase.GetAssetPath(prefabToTest.prefab)+" has an external dependency." +
+                        Environment.NewLine+"The dependency is "+asset.name+" and it should be located inside "+prefabToTest.constraintPath+" or at a external resource path."+
+                        Environment.NewLine+"The dependency is located at "+AssetDatabase.GetAssetPath(asset)+" for now");
 
         private T[] FindAssetsByType<T>() where T : Object =>
             AssetDatabase.FindAssets($"t:{typeof(T)}")
